2275654646da04c0451b2c9dbd4d261d99322621
a-alca <a.alca@icloud.com>
2023-05-31 13:54:07 -0300

    Anotacoes sobre callbacs e retorno de callback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,15 +19,8 @@ const sendMsg = () => {
 setTimeout(sendMsg, 5000);
 console.log("Bran diz: tks xP")
 
-    // expected output:
-                        //"Messages"
-                        //"Bran diz: tks xP") << TASk QUEUE
-                        //"Bran say: Hi, how are you"
-                        //"Bran say: I`ll send you a invitation!"
-                        //"Joojen answer: got it!"
-
-                        // A funcao setTimeout é responsavel por fazer que uma funcao agurade um determinado momento a ser executada enquanto outras funcoes estao sendo executadas, ela fica em segundo plano. O exemplo de lavar a louça é perfeito, pois por norma mundial, primeiro são lavados os copos >> colocamos agua com detergente dentro das panelas e potes e deixamos ESPERANDO la em cima do fogão >> enquanto isso, eu nao para de lavar a louça, volto para lavar os talheres, pratos, e por fim, executo o lavar as panelas e potes.
-
-                        // entao, diferente do modo SINCRONO onde a leitura funcionou através do EVENT LOOP e do CALL STACK, o modo ASINCRONO, quando Event Loop recebe no Call Stack uma funcao setTimeout, ele guarda na TASK QUEUE,  agurada lá por debaixo dos panos, executa o que sincrono e por fim executa a funcao que esta na task queue.
-
-                        // Essa é um exmplo da FORMA SINCRONA onde: O EVENT LOOP é como um seguranca na festa e vai guiando para qual porta a pessoa tem que entrar, qual o acesso dela e quando ela deve acontecer. Assim, ele fica o tempo inteiro no codigo olhando o que vai ser chamado para a CALL STACK. Já na Call Stack vao todas as coisas que devem acontecer na tela. Entao ele chama uma coisa por vez no código, essa é a funcao dele. O EVENT LOOP manda para lá o nosso c[odigo, as partes "VIPs" que tem que acontecer primeiro. elas entram primeiro ali na fila. Por padrão, tudo vem vazio. Mas tambem temos a TASK QUEUE, que vai dar conta do nosso sistema síncrono (é a outra fila que o guarda vai fazer a segurança).
+// CALLBACK:
+            //Callbacks são, basicamente, funções envidas como parâmetro para outras funçoes.
+            // Como no codigo acima, a funcao assincrona setTimeout recebe a funcao sendMsg e o callback transofmra essa execucao da funcao sendMsg em cinco segundos apos o inicio do codigo.
+            // Entao, aconteceu uma tarefa em segundo plano, que nao interrompeu o andamento das funcoes posteriores dos outros console.log. E isso facilita e agiliza muito o nosso trabalho, por exemplo, com as requisicoes.
+            // Geralmente, callbacks sao executados quando alguma operacao é conluida ou quando um evento espec[ifico ocorre. Callbacks sao assincronos, portanto sao funcoes que sao ativadas por algum fator predeterminado, podendo ser um tempo especifico, a partir de uma acao do usuario, depois da conclusao de alguma coisa.
