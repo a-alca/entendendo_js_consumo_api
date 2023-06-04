0d4addbfeb15ce36d3482488a8be07a9b4ee1502
a-alca <a.alca@icloud.com>
2023-06-04 00:22:15 -0300

    ASYNC AWAIT - tratamento de erros + BACKLOG de melhoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,22 @@ export class AppComponent {
 }
 
 async function buscaEdereco() { //criando funcao assincrona
+  try {
 
-  var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
-  var consultaCepConvertida = await consultaCEP.json(); // pega o json da api e converte nos dados de endereco do cep
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
+    var consultaCepConvertida = await consultaCEP.json(); // pega o json da api e converte nos dados de endereco do cep
+    if (consultaCepConvertida.erro) {
+      throw Error ('CEP inexistente!') // quando informamos um `cep` com 8 digitos numericos mas inexistente o viacep retorna erro = True e por isso pfoi preciso tratar nessa linha. Quando for digitado em formato inválido, o viacep retorna erro 400, e para isso, encaminharei para backlog de melhoria uma solucao para tratar que no input 'cep' o usuario so consiga digitar numeros e que o botao "consultar CEP" s[o fique ativo apos digitar 8 numeros.
+    }
+    console.log(consultaCepConvertida) // imprimi object com bairrro; cep; complemento... /...
 
-  console.log(consultaCepConvertida) // imprimi object com bairrro; cep; complemento... /...
+  } catch (erro) {
+    console.log(erro)
+  }
+
 
 }
 
 buscaEdereco(); // puxa a funcao e coloca la na callstack
 
-                // Nao existe so uma maneira de escrever um codigo assincrono, para evitar o chamado callbackhell (onde haveria vários THEN, como na consulta cep onde eu dou um FETCH na API, THEN resposta.json. THEN trata erro, THEN cada item do objeto) podemos criar uma AWAIT FUNCTION, onde consumimos uma API e tratamos suas RESPONSES.
+                // try >> catch - throw ERROR
