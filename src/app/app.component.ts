75a6b08f835e78a41b925e0fc233cf077dcf314d
a-alca <a.alca@icloud.com>
2023-06-04 15:25:43 -0300

    bonus - Manipulando formualarios - CONSULTA DINAMICA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myApp';
-}
 
-async function buscaEdereco(cep) { //criando funcao assincrona
-  try {
+  constructor() {};
 
-    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    var consultaCepConvertida = await consultaCEP.json(); // pega o json da api e converte nos dados de endereco do cep
-    if (consultaCepConvertida.erro) {
-      throw Error ('CEP inexistente!')
+  ngOnInit(): void {
+    async function buscaEdereco(cep) { //criando funcao assincrona
+      try {
+
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCEP.json(); // pega o json da api e converte nos dados de endereco do cep
+        if (consultaCepConvertida.erro) {
+          throw Error ('CEP inexistente!')
+        }
+        console.log(consultaCepConvertida); // imprimi object com bairrro; cep; complemento... /...
+        return consultaCepConvertida;
+
+      } catch (erro) {
+        console.log(erro)
+      }
     }
-    console.log(consultaCepConvertida); // imprimi object com bairrro; cep; complemento... /...
-    return consultaCepConvertida;
 
-  } catch (erro) {
-    console.log(erro)
+//promise.all
+
+      //let ceps = ['09195350', '010010001'];
+      //let conjuntoCeps = ceps.map(valores => buscaEdereco(valores));
+      //console.log(conjuntoCeps)
+      //Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+
+//consulta dinamica aula Alura - nao estava conseguindo getElementById
+
+      //var cep = document.getElementById('cep');
+      //cep.addEventListener("focusout", () => buscaEdereco(cep.value))
+
+//consulta dinamica solucionada neste meu estudo
+
+    var cep = (<HTMLInputElement>document.getElementById('cep'));
+    cep.addEventListener("focusout", () => buscaEdereco(cep.value))
+
+    }
   }
-}
 
-let ceps = ['09195350', '010010001'];
-let conjuntoCeps = ceps.map(valores => buscaEdereco(valores));
-console.log(conjuntoCeps)
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+                // CONSULTA DINAMICA
 
+                    //Para a consulta dinamica, consulta o CEP apos digitar e clicar fora do campo CEP, foi construido o codigo comentado na linnha 39/40, criou-se uma variavel cep que recebia o id cep do documento html e pegavamos esse elemendo adicionando um escutador de eventos e ao escutar que clicavamos fora do campo CEP era feita a funcao assincrona buscaEndereco que recebia o valor de cep.
 
-                // PROMISE ALL
+                    //Porem o "problema" é que o typeScript é typesafe, entao o document.getElementByID() retorna o tipo HTMLElement que nao contem a propriedade de value. O subtipo HTMLInputElement tambem nao contem o valor da propriedade.
 
-                    // Nao é o caso de resolucao dessa atividade, mas para entender o PROMISE.ALL, foi apresentado o desafio caso seja necessário buscar mais de um cep na mesma chamada: E para isso, a funcao assincrona buscaEnderco precisa receber um valor de parametro, vou inserir o valor de parametro "cep" >> no fetch api, troquei as aspas simples por crase >> Onde tinha o valor do CEP, inserir um ${cep} (dentro delas é colocado o valor esperado receber no parametro cep).
-
-                    // Apos esses passos, inserir um retorno abaixo do retorno console.log(consultaConvertida) que ele vai chamar para quem tiver chamando essa funcao, esse valor.
-
-                    // depois, no fianl, antes da chamada da funcao assincrona BuscaEndereco(), inseri um LET ceps e um ARRAY de ceps para consulta, e apos isso, fazer um array de conjunto de ceps e para isso, informo o parametro ceps.map e dentro do map é feita uma arrow function com a funcao valores pegar a funcao buscaEndereco() recebendo o valores que estamos pegando = buscaEndereco(valores): Com isso ele faz um novo array com o que retornar daquela funcao buscaEndereco para cada um dos valores de dentro do CEP e esses valores serao PROMISES e para resolver mais de uma promessa, é utilizado o PROMISE.ALL.
-
-                    // entao tratando com promise.all, nessa chamada vemos no console os dois objetos tratado com os dados de enderco dos respectivos ceps informados
-
-                    //O async/await apesar de ser uma opção mais "legível" ao .then() é importante frisar que não são logicamente equivalentes: o async/await faz o processamento de forma sequencial, Promises com .then() são processadas em paralelo, o que faz com que este método seja mais rápido. O async/await simplifica a escrita e a interpretação do código, mas não é tão flexível e só funciona com uma Promise por vez. O artigo “Async/await no JavaScript: o que é e quando usar a programação assíncrona?” pode te ajudar a entender ambos os casos e suas diferenças
+                    //A solucao para isso é lancar o resultado do getElementByID para o HTMLInputElement criando a variavel cep que recebe o <HTMLInputElement>document.getElementById('cep') e ai sim incrementamos um escutador de eventos no campo cep com o focusout para executar a funcao assincrona busca endereco com o valor de cep.
